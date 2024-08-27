d7a17c4984e9ee87de93ca1e15d8cf0b7e46fc4b
Kritika <135961523+30kritika@users.noreply.github.com>
2024-08-27 18:28:43 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Hero from './assets/components/Hero/Hero'
+
+function App() {
+
+  return (
+    <div className='overflow-x-hidden'>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
